56cc2f0a5e237d04e4214f8a63cf3298c3c325fc
juras76 <juraktomasz@gmail.com>
2020-03-27 13:28:20 +0100

    Autoryzacja i dostep API do danych

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
-{
+{   
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -29,6 +31,7 @@ namespace PortalRandkowy.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
